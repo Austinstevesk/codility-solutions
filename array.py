3c53f83151d5d6c93ecfab649dcf215ea5934ecf
Austinstevesk <austinstevesk@gmail.com>
2021-01-27 21:45:03 +0300

    Added an array solution

---
+++ array.py
@@ -0,0 +1,32 @@
+"""
+Function taking 2 arrays A and B, if a value 
+"""
+
+"""def solution(A,B):
+	list1 = []
+	for i in A:
+		if i in B:
+			list1.append(i)
+			print(list1)
+	if list1 == []:
+		return -1
+	else:
+		return min(list1)"""
+
+def solution(A, B):
+    A.sort()
+    B.sort()
+    i = 0
+    for a in A:
+        if i < len(B) - 1 and B[i] < a:
+            i += 1
+        if a == B[i]:
+        	print(a)
+    if a != B[i]:
+    	print(-1)
+        
+
+
+X = [1,3,2,1]
+Y = [0,2,5,3,2]
+solution(X,Y)
