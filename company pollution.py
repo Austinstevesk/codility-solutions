a4d107be5ecf2dd29eb56ed745fca8fd5982c316
Austinstevesk <austinstevesk@gmail.com>
2021-01-28 19:10:49 +0300

    Added company emissions

--- company pollution.py
+++ company pollution.py
@@ -3,6 +3,9 @@ An industrial company has N factories each producing some pollution every month.
 reduce the fume emissions using one or more filters. Every such filter reduces the pollution by half. 
 When the second(subsequent) filter is applied, it again reduces by half the remaining pollution 
 emitted after fitting the existing filter(s). You are given an array of N integers describing the
-amount of pollution produced by the factories. Your tas is to find the maximum number of filters needed
+amount of pollution produced by the factories. Your tas is to find the minimum number of filters needed
 to decrease the total sum of pollution by at least half.
+
+Which given an array of integers A of length N, returns the minimum number of filters needed to reduce
+the total pollution by half
 """
