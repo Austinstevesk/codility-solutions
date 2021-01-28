8653be69004e64309e00e0ded805b0c1db5b02b6
Austinstevesk <austinstevesk@gmail.com>
2021-01-28 19:04:20 +0300

    Added company emissions

---
+++ company pollution.py
@@ -0,0 +1,6 @@
+"""
+An industrial company has N factories each producing some pollution every month. The company decided to
+reduce the fume emissions using one or more filters. Every such filter reduces the pollution by half. 
+When the second(subsequent) filter is applied, it again reduces by half the remaining pollution 
+emitted after fitting the existing filter(s).
+"""
